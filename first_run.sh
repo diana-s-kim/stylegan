26524af730950dc6a3dc306fa3c9d6e60b93fb8a
Diana Kim <62619794+diana-s-kim@users.noreply.github.com>
2023-06-07 14:20:41 -0400

    Create first_run.sh
    make runnable on ibex

---
+++ first_run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash                                                                                                                                                                                   
+
+module load cudnn/7.5.0-cuda10.1.105
+module unload cuda/10.1.105
+moudle load cuda/10.0.130
